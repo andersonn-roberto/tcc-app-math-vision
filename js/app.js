f8bf6672c56f81f6803a421023341920b297c346
Anderson Roberto de Oliveira <andersonn.roberto@gmail.com>
2020-06-28 16:08:50 -0300

    Tela de login e plotando grafico quando tela muda
    Inclusao de tela de login (ainda nao funcional)
    Plotando o grafico novamente quando a pagina/tela eh redimensionada

--- js/app.js
+++ js/app.js
@@ -1,34 +1,41 @@
 function draw() {
-    try {
-        // compile the expression once
-        const expression = document.getElementById('eq').value
-        const expr = math.compile(expression)
+  try {
+    // compile the expression once
+    const expression = document.getElementById("eq").value;
+    const expr = math.compile(expression);
 
-        // evaluate the expression repeatedly for different values of x
-        const xValues = math.range(-10, 10, 0.5).toArray()
-        const yValues = xValues.map(function (x) {
-            return expr.evaluate({
-                x: x
-            })
-        })
+    // evaluate the expression repeatedly for different values of x
+    const xValues = math.range(-10, 10, 0.5).toArray();
+    const yValues = xValues.map(function (x) {
+      return expr.evaluate({
+        x: x,
+      });
+    });
 
-        // render the plot using plotly
-        const trace1 = {
-            x: xValues,
-            y: yValues,
-            type: 'scatter'
-        }
-        const data = [trace1]
-        Plotly.newPlot('plot', data)
-    } catch (err) {
-        console.error(err)
-        alert(err)
-    }
+    // render the plot using plotly
+    const trace1 = {
+      x: xValues,
+      y: yValues,
+      type: "scatter",
+      mode: "lines",
+      line: {
+        color: "rgb(55, 128, 191)",
+        width: 3,
+      },
+    };
+    const data = [trace1];
+    Plotly.newPlot("plot", data);
+  } catch (err) {
+    console.error(err);
+    alert(err);
+  }
 }
 
-document.getElementById('form').onsubmit = function (event) {
-    event.preventDefault()
-    draw()
-}
+document.getElementById("form").onsubmit = function (event) {
+  event.preventDefault();
+  draw();
+};
 
-draw()
+window.onresize = draw;
+
+draw();
