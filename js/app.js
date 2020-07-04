99ad6ab40aea6f136319ef028e6c8d817a572ee6
Anderson Roberto de Oliveira <andersonn.roberto@gmail.com>
2020-07-04 19:17:20 -0300

    Tela de login e alterando configuracoes
    Tela de login separada, mockada, não funciona no github pages
    Configurações funcionando perfeitamente

--- js/app.js
+++ js/app.js
@@ -19,11 +19,14 @@ function draw() {
       type: "scatter",
       mode: "lines",
       line: {
-        color: "rgb(55, 128, 191)",
+        color: slcCorLinha.value,
         width: 3,
       },
     };
     const data = [trace1];
+    const layout = {
+      plot_bgcolor: "rgb(0,0,0,0)",
+    };
     Plotly.newPlot("plot", data);
   } catch (err) {
     console.error(err);
@@ -31,11 +34,44 @@ function draw() {
   }
 }
 
+var btnSalvar = document.getElementById("btn-salvar-configuracoes");
+var slcCorFundo = document.getElementById("cor_background");
+var slcCorFonte = document.getElementById("cor_foreground");
+var slcCorLinha = document.getElementById("cor_linha");
+
+if (window.localStorage.getItem("cor_background") !== null) {
+  slcCorFundo.value = window.localStorage.getItem("cor_background");
+} else {
+  slcCorFundo.value = "rgb(255, 255, 255)";
+}
+document.getElementsByTagName("body")[0].style.backgroundColor =
+  slcCorFundo.value;
+
+if (window.localStorage.getItem("cor_foreground") !== null) {
+  slcCorFonte.value = window.localStorage.getItem("cor_foreground");
+} else {
+  slcCorFonte.value = "rgb(0, 0, 0)";
+}
+document.getElementsByTagName("body")[0].style.color = slcCorFonte.value;
+
+if (window.localStorage.getItem("cor_linha") !== null) {
+  slcCorLinha.value = window.localStorage.getItem("cor_linha");
+} else {
+  slcCorLinha.value = "rgb(0, 0, 205)";
+}
+
+btnSalvar.addEventListener("click", function () {
+  window.localStorage.setItem("cor_background", slcCorFundo.value);
+  window.localStorage.setItem("cor_foreground", slcCorFonte.value);
+  window.localStorage.setItem("cor_linha", slcCorLinha.value);
+  window.location.reload();
+});
+
+draw();
+
 document.getElementById("form").onsubmit = function (event) {
   event.preventDefault();
   draw();
 };
 
 window.onresize = draw;
-
-draw();
