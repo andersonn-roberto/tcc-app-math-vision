52675b5e791c529ceb6e1748acef11fd5b415df4
Anderson Roberto de Oliveira <andersonn.roberto@gmail.com>
2020-07-12 10:01:22 -0300

    Posicao das preferencias; Libs locais;
    Alterada a posicao das preferencias para o topo da pagina
    Adicionado botao para exibir/ocultar as preferencias
    Usando libs locais

--- js/app.js
+++ js/app.js
@@ -34,38 +34,95 @@ function draw() {
   }
 }
 
-var btnSalvar = document.getElementById("btn-salvar-configuracoes");
-var slcCorFundo = document.getElementById("cor_background");
-var slcCorFonte = document.getElementById("cor_foreground");
-var slcCorLinha = document.getElementById("cor_linha");
+var show = function (elem) {
+	elem.style.display = 'block';
+};
 
-if (window.localStorage.getItem("cor_background") !== null) {
-  slcCorFundo.value = window.localStorage.getItem("cor_background");
+var hide = function (elem) {
+	elem.style.display = 'none';
+};
+
+var toggle = function (elem) {
+
+	// If the element is visible, hide it
+	if (window.getComputedStyle(elem).display === 'block') {
+		hide(elem);
+		return;
+	}
+
+	// Otherwise, show it
+	show(elem);
+
+};
+
+const ultimoLogin = window.localStorage.getItem("ultimo_login");
+
+if (ultimoLogin == null) {
+  window.location.assign(window.location.origin + "/login.html");
+}
+
+let preferencias = window.localStorage.getItem(ultimoLogin);
+
+const btnSalvar = document.getElementById("btn-salvar-configuracoes");
+const slcCorFundo = document.getElementById("cor_background");
+const slcCorFonte = document.getElementById("cor_foreground");
+const slcCorLinha = document.getElementById("cor_linha");
+const slcTamanhoFonte = document.getElementById("tamanho_fonte");
+const btnSair = document.getElementById("btn-sair");
+const btnShowHide = document.getElementById("btn-show-hide");
+
+if (preferencias && JSON.parse(preferencias).cor_background !== null) {
+  slcCorFundo.value = JSON.parse(preferencias).cor_background;
 } else {
   slcCorFundo.value = "rgb(255, 255, 255)";
 }
 document.getElementsByTagName("body")[0].style.backgroundColor =
   slcCorFundo.value;
 
-if (window.localStorage.getItem("cor_foreground") !== null) {
-  slcCorFonte.value = window.localStorage.getItem("cor_foreground");
+if (preferencias && JSON.parse(preferencias).cor_foreground !== null) {
+  slcCorFonte.value = JSON.parse(preferencias).cor_foreground;
 } else {
   slcCorFonte.value = "rgb(0, 0, 0)";
 }
 document.getElementsByTagName("body")[0].style.color = slcCorFonte.value;
 
-if (window.localStorage.getItem("cor_linha") !== null) {
-  slcCorLinha.value = window.localStorage.getItem("cor_linha");
+if (preferencias && JSON.parse(preferencias).cor_linha !== null) {
+  slcCorLinha.value = JSON.parse(preferencias).cor_linha;
 } else {
   slcCorLinha.value = "rgb(0, 0, 205)";
 }
 
+if (preferencias && JSON.parse(preferencias).tamanho_fonte !== null) {
+  slcTamanhoFonte.value = JSON.parse(preferencias).tamanho_fonte;
+} else {
+  slcTamanhoFonte.value = "1em";
+}
+document.getElementsByTagName("body")[0].style.fontSize = slcTamanhoFonte.value;
+
 btnSalvar.addEventListener("click", function () {
-  window.localStorage.setItem("cor_background", slcCorFundo.value);
-  window.localStorage.setItem("cor_foreground", slcCorFonte.value);
-  window.localStorage.setItem("cor_linha", slcCorLinha.value);
+  preferencias = {
+    cor_background: slcCorFundo.value,
+    cor_foreground: slcCorFonte.value,
+    cor_linha: slcCorLinha.value,
+    tamanho_fonte: slcTamanhoFonte.value,
+  };
+  window.localStorage.setItem(ultimoLogin, JSON.stringify(preferencias));
   window.location.reload();
 });
+
+btnSair.addEventListener("click", function () {
+  window.localStorage.setItem("ultimo_login", null);
+  window.location.assign(window.location.origin + "/login.html");
+});
+
+btnShowHide.addEventListener("click", function () {
+  // Get the content
+	var content = document.querySelector("#configuracoes");
+	if (!content) return;
+
+	// Toggle the content
+	toggle(content);
+})
 
 draw();
 
